3781eecf829885d402ba45021b127601660a3b95
BeckySayer <rsayer532@gmail.com>
2023-05-02 16:00:57 +0100

    Working with tasks

--- Filter/Program.cs
+++ Filter/Program.cs
@@ -5,65 +5,73 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Net.Mail;
+using System.Timers;
+using System.Reflection.PortableExecutable;
+using System.Diagnostics;
 
 class Program
 {
-
+    private static Stopwatch stopwatch = new Stopwatch();
     private static Mail testMail; //this should be new Mail object each time written to.
 
     private static Filter filter = Filter.Instance;
 
     private static List<Mail> mailCollect = new List<Mail>();
-    
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
-
+        stopwatch.Start();
         Filter filter = Filter.Instance;
+        Init();
+        Start();
+        RandomUpdate();
+        PrintMail();
+    }
+    
+    public static void Init()
+    {
         filter.UpdatePath(Filter.Mode.initialise);
-
-
-        Thread Thread1 = new Thread(new ThreadStart(Start));
-        //Thread Thread2 = new Thread(new ThreadStart(CollectMail));
-        Thread Thread3 = new Thread(new ThreadStart(RandomUpdate));
-
-        Thread1.Start();
-        Thread3.Start();
-        
-        //var task1 = Start();
-        //var task2 = RandomUpdate();
-        //Task.WaitAny(task1, task2);
-
     }
 
-    public static void Start()
+    public static async void Start()
     {
-       
-        filter.UpdatePath(Filter.Mode.initialise);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 30; i++)
+        {
+            Thread.Sleep(1000);
+            await Task.Run(() =>
+            {
+                filter.CallPositionUpdate();
+                filter.Output(out testMail);
+                mailCollect.Add(testMail);
+            });
+        }
+     
+        
+    }
+
+    public static async void RandomUpdate()
+    {
+      
+        for (int i = 0; i < 30; i++)
+        {
+            Thread.Sleep(500);
+            await Task.Run(() => { filter.UpdatePath(Filter.Mode.update); });
+            //Console.WriteLine("Random update made");
+        }       
+    }
+    private static void PrintMail()
+    {
+
+        for (int i = 0; i < 30; i++)
         {
             Thread.Sleep(2000);
-            // filter.UpdatePath(Filter.Mode.update);
-            filter.CallPositionUpdate();
-            filter.Output(out testMail);
-            Console.WriteLine($"Range is: {testMail.Range}");
-            Console.WriteLine($"Bearing is: {testMail.Bearing}");
-            //mailCollect.Add(testMail);          
+            
+            Console.WriteLine($"Time: {stopwatch.Elapsed}");
+            Console.WriteLine($"Range: {mailCollect.Last().Range}");
+          //  Console.WriteLine($"Bearing: {mailCollect.Last().Bearing}");
+            
         }
-        
-    }
 
-    public static void RandomUpdate()
-    {
-        // Random randomTimer = new Random();
-        
-        for (int i = 0; i < 10; i++)
-        {
-            Thread.Sleep(1000 );
-            filter.UpdatePath(Filter.Mode.update);
-            Console.WriteLine("Random update made");
-        }
-        
     }
 
    
